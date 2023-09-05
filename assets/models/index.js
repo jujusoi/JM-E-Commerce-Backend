9a7f4499c8cb5b2f3c53a945c60f23e128ac2760
jujusoi <jalxmcdonald@hotmail.com>
2023-09-05 12:10:36 +0930

    finished table FK and PK connections, has many and many to many

---
+++ assets/models/index.js
@@ -0,0 +1,31 @@
+const Category = require('./category');
+const Product = require('./product');
+const Tag = require('./tag');
+const ProductTag = require('./producttag');
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+}),
+
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, 
+    { 
+    through: ProductTag,
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE',
+    }
+);
+
+Tag.belongsToMany(Product, 
+    { 
+    through: ProductTag,
+    foreignKey: 'tag_id',
+    onDelete: 'CASCADE',
+    }
+);
+
+module.exports = { Category, Product, Tag, ProductTag };
