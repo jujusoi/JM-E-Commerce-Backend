f040f7448766d8095a58d1a53e021387257ffa32
jujusoi <jalxmcdonald@hotmail.com>
2023-09-06 13:05:18 +0930

    added category put and delete requests, category route finished

--- assets/routes/api/categoryRoutes.js
+++ assets/routes/api/categoryRoutes.js
@@ -12,9 +12,9 @@ category.get('/', async (req, res) => {
      }
  });
  
- category.get('/:num', async (req, res) => {
+ category.get('/:id', async (req, res) => {
     try {
-        const categoryData = await Category.findByPk(req.params.num, {
+        const categoryData = await Category.findByPk(req.params.id, {
             include: [{ model: Product }],
         });
         if (!categoryData) {
@@ -24,6 +24,46 @@ category.get('/', async (req, res) => {
         }
     } catch (err) {
         res.status(500).json(`Could not respond with data, ${err}`);
+    }
+});
+
+category.post('/', async (req, res) => {
+    try {
+        const categoryData = await Category.create(req.body);
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(400).json(`Could not post information! ${err}`);
+    }
+});
+
+category.delete('/:id', async (req, res) => {
+    try {
+        const categoryData = await Category.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(`Could not remove information from database, ${err}`);
+    }
+});
+
+category.put('/:id', async (req, res) => {
+    try {
+        const categoryData = await Category.findByPk(req.params.id);
+        if (!categoryData) {
+            res.status(404).json(`Table to update not found`);
+        } else {
+        categoryData.set({
+            category_name: req.body.category_name,
+        });
+        await categoryData.save();
+        res.status(200).json(categoryData);
+        }
+    }
+    catch (err) {
+        res.status(500).json(`Cannot update database information, ${err}`);
     }
 })
 
