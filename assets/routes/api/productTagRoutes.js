91843bbcf1bbf07436397e6861a49b9de46636cf
jujusoi <jalxmcdonald@hotmail.com>
2023-09-07 11:49:17 +0930

    quickly added product tag get and post routes so that users can link newly created or updated products to categories.

--- assets/routes/api/productTagRoutes.js
+++ assets/routes/api/productTagRoutes.js
@@ -1,11 +1,23 @@
 const productTag = require('express').Router();
+const { ProductTag } = require('../../models');
 
 productTag.get('/', async (req, res) => {
-    try { res.status(200).json(`Get request to product tags`);
+    try { 
+        const productTagData = await ProductTag.findAll();
+        res.status(200).json(productTagData)
      } catch (err) {
          res.status(500).json(`Failed, ${err}`);
      }
  });
+
+ productTag.post('/', async (req, res) => {
+    try {
+        const productTagData = await ProductTag.create(req.body);
+        res.status(200).json(productTagData);
+    } catch (err) {
+        res.status(500).json(`Could not insert value into database, ${err}`);
+    }
+ })
 
  module.exports = productTag;
  
--- assets/routes/api/tagRoutes.js
+++ assets/routes/api/tagRoutes.js
@@ -27,5 +27,14 @@ tag.get('/', async (req, res) => {
     }
  });
 
+ tag.post('/', async (req, res) => {
+    try {
+        const tagData = await Tag.create(req.body);
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.status(500).json(`Could not insert value into database, ${err}`);
+    }
+ })
+
  module.exports = tag;
  
